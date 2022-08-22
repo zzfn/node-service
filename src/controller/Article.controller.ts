9cfa9d639a04df155f701f3ded491acb18684791
cc <44751363+zzfn@users.noreply.github.com>
2022-08-22 21:51:05 +0800

    Changes

--- src/controller/Article.controller.ts
+++ src/controller/Article.controller.ts
@@ -3,6 +3,7 @@ import {ArticleService} from '../service/Article.service';
 import {PageVo} from '../vo/PageVo';
 import {Article} from "../entity/Article";
 import {ElasticsearchService} from '@midway/elasticsearch';
+import {ResultUtil} from "../util/ResultUtil";
 
 @Controller('/article')
 export class APIController {
@@ -28,16 +29,31 @@ export class APIController {
       index: 'blog',
       body: {
         query: {
-          bool:
-            {must: [{match: {content: keyword}}]}
+          bool: {
+            should: [
+              {match_phrase: {content: keyword}},
+              {match_phrase: {title: keyword}},
+              {match_phrase: {tag_desc: keyword}},
+            ]
+          }
         },
         highlight: {
-          fields : {
-            content : {}
+          fields: {
+            content: {},
+            title: {},
+            tag_desc: {},
           }
         }
       }
     })
-    return result.body;
+    const response = result.body.hits.hits.filter(hit=>hit._source.is_release===1&&hit._source.is_delete===0).map(hit =>
+      Object.entries(hit.highlight as Record<string, string[]>).reduce((prev, curr) => {
+        if (curr[1].length) {
+          prev[curr[0]] = curr[1].join(" ")
+        }
+        return prev
+      }, {})
+    )
+    return ResultUtil.success(response);
   }
 }
