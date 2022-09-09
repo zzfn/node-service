bcb394c8371477bc71cd5ba1c81101d79d8a591f
orluna <44751363+zzfn@users.noreply.github.com>
2022-09-10 01:17:37 +0800

    feat: file

--- src/controller/File.controller.ts
+++ src/controller/File.controller.ts
@@ -1,5 +1,6 @@
 import {
   Controller,
+  Fields,
   Files,
   Get,
   Inject,
@@ -14,10 +15,14 @@ export class APIController {
   ossService: OSSService;
 
   @Post('/upload')
-  async upload(@Files() files) {
+  async upload(@Files() files, @Fields() fields) {
+    let path = 'midway';
+    if (fields.path) {
+      path = fields.path;
+    }
     const result = await Promise.all(
       files.map(file =>
-        this.ossService.put(`midway/${file.filename}`, file.data)
+        this.ossService.put(`${path}/${file.filename}`, file.data)
       )
     );
     return result.map(file => file.url);
