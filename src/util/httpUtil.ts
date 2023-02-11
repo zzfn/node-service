02dbcc7a935a40bb92364a78e95a033fe0452af4
orluna <44751363+zzfn@users.noreply.github.com>
2023-02-11 21:30:43 +0800

    feat: up mysql2

--- src/util/httpUtil.ts
+++ src/util/httpUtil.ts
@@ -2,8 +2,9 @@ import { Context } from '@midwayjs/koa';
 
 export function getUserIp(ctx: Context): string {
   const ip = (ctx.headers['x-forwarded-for'] as string) || ctx.request.ip;
-  return ip === '::1' ? '127.0.0.1' : ip;
+  return process.env.NODE_ENV === 'local' ? '127.0.0.1' : ip;
 }
+
 export function getValueFromHeader(ctx: Context, field: string): string {
   return ctx.headers[field.toLowerCase()] as string;
 }
