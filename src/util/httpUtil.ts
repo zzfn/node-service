97dcc4a03bd6106a3aa54fb356b1f1deee00c9e3
orluna <44751363+zzfn@users.noreply.github.com>
2023-03-28 17:47:08 +0800

    feat: ip

--- src/util/httpUtil.ts
+++ src/util/httpUtil.ts
@@ -1,8 +1,8 @@
 import { Context } from '@midwayjs/koa';
 
 export function getUserIp(ctx: Context): string {
-  // const ip = (ctx.headers['x-forwarded-for'] as string) || ctx.request.ip;
-  return process.env.NODE_ENV === 'local' ? '127.0.0.1' : '';
+  const ip = (ctx.headers['x-forwarded-for'] as string) || ctx.request.ip;
+  return process.env.NODE_ENV === 'local' ? '127.0.0.1' : ip;
 }
 
 export function getValueFromHeader(ctx: Context, field: string): string {
