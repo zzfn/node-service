63be073a91984478204c9983fb6a86874016f8e8
zzfn <44751363+zzfn@users.noreply.github.com>
2023-04-02 12:42:32 +0800

    feat: base

--- src/service/BaseService.ts
+++ src/service/BaseService.ts
@@ -2,15 +2,42 @@ import { Repository } from 'typeorm';
 import { FindOptionsWhere } from 'typeorm/find-options/FindOptionsWhere';
 import { FindOptionsOrder } from 'typeorm/find-options/FindOptionsOrder';
 import { BaseEntity } from '../entity/BaseEntity';
-
+import { Provide } from '@midwayjs/core';
+@Provide()
 export abstract class BaseService<T extends BaseEntity> {
   abstract getModel(): Repository<T>;
+  public abstract entity: Repository<T>
+
+  add(query) {
+    return this.entity.save(query)
+  }
+  update(query) {
+    return this.entity.update(query.id, query)
+  }
+  delete(ids: number | string | string[]) {
+    return this.entity.delete(ids)
+  }
+  info(data) {
+    return this.entity.findOne({ where: data })
+  }
+  async page1(data) {
+    const { page = 1, size = 10 } = data
+    const [list, total] = await this.entity.findAndCount({
+      where: {},
+      take: size,
+      skip: (page - 1) * size
+    })
+    return { list, pagination: { total, size, page } }
+  }
+  list1(data?) {
+    return this.entity.find({ where: data } as any)
+  }
 
   async page(
     where: FindOptionsWhere<T>,
     pageNo: number,
     pageSize: number
-  ): Promise<any> {
+  ) {
     const skip = (pageNo - 1) * pageSize;
     const take = pageSize;
     const [records, total] = await this.getModel().findAndCount({
