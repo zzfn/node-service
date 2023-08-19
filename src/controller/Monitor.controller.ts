75fc9982978996ce23594322ca0b054f80d31949
cw <44751363+zzfn@users.noreply.github.com>
2023-08-19 17:45:21 +0800

    feat: monitor

---
+++ src/controller/Monitor.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Inject } from '@midwayjs/decorator';
+import { CommentService } from '../service/Comment.service';
+import { makeHttpRequest } from '@midwayjs/core';
+
+@Controller('/monitor')
+export class CommentController {
+  @Inject()
+  commentService: CommentService;
+
+  @Get('/status')
+  async status(): Promise<any> {
+    const { data: statusTarget }: any = await makeHttpRequest(
+      `${process.env.PROM_URL}/api/v1/query_range?query=probe_success&start=${
+        Date.now() / 1000 - 60 * 60 * 24
+      }&end=${Date.now() / 1000}&step=${60 * 60}`,
+      {
+        method: 'GET',
+        dataType: 'json',
+      }
+    );
+    const { data: nameTarget }: any = await makeHttpRequest(
+      `${
+        process.env.PROM_URL
+      }/api/v1/targets/metadata?metric=probe_success&t=${Date.now()}`,
+      {
+        method: 'GET',
+        dataType: 'json',
+      }
+    );
+    const hashMap = new Map();
+    nameTarget.data
+      .map((_: any) => _.target)
+      .forEach((_: any) => {
+        hashMap.set(_.instance, _.name);
+      });
+    return statusTarget.data.result.map(_ => {
+      return {
+        ..._,
+        metric: {
+          ..._.metric,
+          name: hashMap.get(_.metric.instance),
+        },
+      };
+    });
+  }
+}
