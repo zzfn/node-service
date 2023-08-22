a656c216c381d60847ff3989b4717cb621296782
cw <44751363+zzfn@users.noreply.github.com>
2023-08-22 13:26:07 +0800

    feat: kafaka

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -1,21 +1,22 @@
 import { Body, Controller, Get, Inject } from '@midwayjs/decorator';
-import { KafkaService } from '../service/KafkaService';
+
+// import { KafkaService } from '../service/KafkaService';
 
 @Controller('/')
 export class HomeController {
   @Inject()
-  kafkaService: KafkaService;
-
+  // kafkaService: KafkaService;
   @Get('/')
   async home(@Body() req: any): Promise<string> {
-    const result = await this.kafkaService.send({
-      topic: 'topic-test0',
-      messages: [
-        {
-          value: JSON.stringify({ messageValue: 'ssssss' }),
-        },
-      ],
-    });
-    return result;
+    // const result = await this.kafkaService.send({
+    //   topic: 'topic-test0',
+    //   messages: [
+    //     {
+    //       value: JSON.stringify({ messageValue: 'ssssss' }),
+    //     },
+    //   ],
+    // });
+    // return result;
+    return '';
   }
 }
