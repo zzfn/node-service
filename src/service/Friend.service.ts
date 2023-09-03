0ae7cf4ec371f93e2b98b106def97a9a7a2548c7
zzfn <44751363+zzfn@users.noreply.github.com>
2023-09-03 14:29:10 +0800

    feat: id

--- src/service/Friend.service.ts
+++ src/service/Friend.service.ts
@@ -1,17 +1,24 @@
 import { Provide } from "@midwayjs/core";
 import { InjectEntityModel } from "@midwayjs/typeorm";
 import { Friend } from "../entity/Friend";
+import { Inject } from "@midwayjs/decorator";
+import { SnowflakeIdGenerate } from "./Snowflake";
 
 @Provide()
 export class FriendService {
   @InjectEntityModel(Friend)
   friendModel;
+  @Inject()
+  idGenerate: SnowflakeIdGenerate;
 
   async list() {
     return await this.friendModel.find({});
   }
 
   async save(friend: Friend) {
+    if (!friend.id) {
+      friend.id = this.idGenerate.nextId().toString();
+    }
     return this.friendModel.save(friend);
   }
 }
